8a968b79910e71fe5c2fea6f9acf5f12f1f773cb
ivanlmj <ivanlmj@gmail.com>
2018-08-26 12:30:05 -0500

    adding file upload option

--- javascript/webserver_prototype/app.py
+++ javascript/webserver_prototype/app.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 
+import os
+
 from flask import Flask, jsonify, render_template, request
+from werkzeug import secure_filename
 
 app = Flask(__name__)
+
+FILE_FOLDER = "/tmp"
+
 
 @app.route("/")
 @app.route("/index")
 def f_index():
     return render_template("index.html")
+
 
 @app.route("/registry", methods = ["GET", "POST"])
 def f_registry():
@@ -19,14 +26,20 @@ def f_registry():
         profession = request.form["profession"]
         email      = request.form["email"]
         country    = request.form["country"]
+        filestore  = request.files["resume"]
+        filename   = secure_filename(filestore.filename)
+        dest = os.path.join(FILE_FOLDER, filename)
+        filestore.save(dest)
         data = {
                 "name":name, 
                 "birthdate":birthdate, 
                 "profession":profession,
                 "email":email,
-                "country":country
+                "country":country,
+                "resume":filename
                 }
         return jsonify(data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
