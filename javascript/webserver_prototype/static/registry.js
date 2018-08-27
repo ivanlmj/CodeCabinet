e327af9badbd707140a209efbd1ec7205b1c8580
ivanlmj <ivanlmj@gmail.com>
2018-08-26 23:52:56 -0500

    adding form validation feature

--- javascript/webserver_prototype/static/registry.js
+++ javascript/webserver_prototype/static/registry.js
@@ -1,3 +1,23 @@
 function welcome() {
     alert("Welcome to User Registry");
 };
+
+function validateForm() {
+    let empty_field = false;
+
+    let inputs = document.forms["form_user"].getElementsByTagName("input");
+    for (index = 0; index < inputs.length; index++) {
+        if (inputs[index].value == null || inputs[index].value == "") {
+	    empty_field = true;
+	    break;
+	}
+    }
+
+    if (empty_field = true) {
+        alert("Please, review empty fields.");
+	return false;
+    } else {
+	return true;
+    }
+
+};
