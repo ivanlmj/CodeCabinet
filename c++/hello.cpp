c7d28f8654b7598da22ac3b7db5d41f5de545553
ivanlmj <ivanlmj@gmail.com>
2018-09-25 12:15:26 -0500

    first exercises with C++

---
+++ c++/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+// Prints a welcome message for the user.
+int main() {
+    cout << "Hello, sucker!\n";
+    return 0;
+}
---
+++ c++/sum_values.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// Prints the amount of required time for leaning C++.
+int main() {
+
+    int num1 = 30;
+    int num2 = 42;
+    int total = num1 + num2;
+
+    cout << "Amount of weeks required for learning C++: " << total << "\n";
+    return 0;
+
+}
