3e46c6afd751cf0f2628c33e645696005302783b
ivanlmj <ivanlmj@gmail.com>
2019-01-22 12:56:40 -0600

    slicing vectors

---
+++ R/vectors_slicing.R
@@ -0,0 +1,9 @@
+
+numbers <- c("apple", "grape", "orange", "papaya", "guaraná")
+
+print(">>> Fruit Vector:")
+print(numbers)
+print(">>> First and Last elements:")
+print(numbers[c(1, 5)])
+print(">>> From third to fifth element:")
+print(numbers[3:5])
