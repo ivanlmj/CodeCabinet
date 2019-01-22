b3bda606e419653ceeecae4093e011880c82f76e
ivanlmj <ivanlmj@gmail.com>
2019-01-22 11:43:32 -0600

    naming vectors (columns)

---
+++ R/vectors_with_names.R
@@ -0,0 +1,10 @@
+# building a vector of populations
+population <- c(1.415, 1.354, 0.327, 0.267, 0.210)
+
+# naming population vector with the country for each population
+names(population) <- c("China", "India", "USA", "Indonesia", "Brazil")
+
+# presenting population vector with names for each vector item
+
+print("Countries and thier Populations (billions).")
+print(population)
