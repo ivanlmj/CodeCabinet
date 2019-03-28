8dfb9ba775160d513106860f8c8ba0ff1ccbdad5
ivanlmj <ivanlmj@gmail.com>
2019-03-27 18:46:03 -0600

    updating PUT and POST parameters (title key)

--- javascript/nodejs/000_express/simple_rest/test_rest_api.sh
+++ javascript/nodejs/000_express/simple_rest/test_rest_api.sh
@@ -8,7 +8,7 @@ read -p "Press any key for the next request..."
 
 # POST
 echo -e "\n> POST"
-curl -X POST --data "task=Code Refactor&user=ivanleoncz" http://localhost:3000/api/v1.0/tasks
+curl -X POST --data "title=Code Refactor&user=ivanleoncz" http://localhost:3000/api/v1.0/tasks
 echo -e "\n"
 read -p "Press any key for the next request..."
 
@@ -26,7 +26,7 @@ read -p "Press any key for the next request..."
 
 # PUT: 5
 echo -e "\n> PUT:    TASK (5)"
-curl -X PUT --data "task=Code Refactor for API&user=ivanleoncz" http://localhost:3000/api/v1.0/tasks/5
+curl -X PUT --data "title=Code Refactor for API&user=ivanleoncz" http://localhost:3000/api/v1.0/tasks/5
 echo -e "\n"
 read -p "Press any key for the next request..."
 
