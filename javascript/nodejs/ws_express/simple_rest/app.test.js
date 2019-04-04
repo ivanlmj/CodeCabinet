a9311d1b23631eb462ae396f2a8dc437e45f3b8f
ivanlmj <ivanlmj@gmail.com>
2019-04-04 11:47:55 -0600

    adding PUT and DELETE tests + improvements
    - tests are ordered in CRUD sequence, also removing one unnecessary test

--- javascript/nodejs/ws_express/simple_rest/app.test.js
+++ javascript/nodejs/ws_express/simple_rest/app.test.js
@@ -2,27 +2,8 @@ const assert = require('assert');
 const request = require('supertest');
 const app = require('./app')
 
-describe('GET: all tasks', function() {
-    test('It should response 200.', (done) => {
-        request(app).get('/api/v1.0/tasks').then(function(response) {
-	    expect(response.statusCode).toBe(200);
-	    done();
-	});
-    });
-});
 
-
-describe('GET: single task', function() {
-    test('It should return response 200.', (done) => {
-        request(app).get('/api/v1.0/tasks/1').then(function(response) {
-	    expect(response.statusCode).toBe(200);
-	    done();
-	});
-    });
-});
-
-
-describe('POST: create new task', function() {
+describe('POST: create new task (id:5)', function() {
     test('It should return response 200.', (done) => {
         request(app).post('/api/v1.0/tasks').send({
 	    title: "Code Refactor",
@@ -34,7 +15,7 @@ describe('POST: create new task', function() {
 });
 
 
-describe('GET: created task', function() {
+describe('GET: created task (id:5)', function() {
     test('It respond with json', function() {
 	return request(app)
 	    .get('/api/v1.0/tasks/5')
@@ -46,3 +27,33 @@ describe('GET: created task', function() {
 	    })
     });
 });
+
+
+describe('PUT: update task (id:5)', function() {
+    test('It should return response 200.', function() {
+        return request(app)
+	    .put('/api/v1.0/tasks/5')
+	    .send({title:'Code Refactor API',user:'ivanleoncz'})
+	    .expect(200);
+    });
+});
+
+
+describe('DELETE: delete task (id:5)', function() {
+    test('It should return response 200', function() {
+        return request(app)
+	    .del('/tasks/5')
+	    .expect(404);
+    });
+});
+
+
+describe('GET: all tasks', function() {
+    test('It should response 200.', (done) => {
+        request(app).get('/api/v1.0/tasks').then(function(response) {
+	    expect(response.statusCode).toBe(200);
+	    done();
+	});
+    });
+});
+
