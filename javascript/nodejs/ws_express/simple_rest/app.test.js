6f175b4f33c4e817c3eebf3ce6ce3644586e42c1
ivanlmj <ivanlmj@gmail.com>
2019-04-04 08:58:26 -0600

    adding test for created task (GET JSON Response)

--- javascript/nodejs/ws_express/simple_rest/app.test.js
+++ javascript/nodejs/ws_express/simple_rest/app.test.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const request = require('supertest');
 const app = require('./app')
 
@@ -24,10 +25,24 @@ describe('GET single task', function() {
 describe('POST/Create new task', function() {
     test('It should return response 200.', (done) => {
         request(app).post('/api/v1.0/tasks').send({
-	    title: "Code  Refactor",
+	    title: "Code Refactor",
 	    user: "ivanleonczBR"
 	}).expect(200).end(function(err, res) {
 	    done();
 	});
+    });
+});
+
+
+describe('GET created task', function() {
+    test('It respond with json', function() {
+	return request(app)
+	    .get('/api/v1.0/tasks/5')
+	    .set('Accept', 'application/json')
+	    .expect('Content-Type', /json/)
+	    .expect(200)
+	    .then(response => {
+		    assert(response.body.title, 'Code Refactor');
+	    })
     });
 });
