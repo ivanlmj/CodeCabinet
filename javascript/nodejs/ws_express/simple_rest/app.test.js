461e4cea9bab917cfbc687630a2af8c72c2277d6
ivanlmj <ivanlmj@gmail.com>
2019-04-04 10:45:31 -0600

    improving test descriptions

--- javascript/nodejs/ws_express/simple_rest/app.test.js
+++ javascript/nodejs/ws_express/simple_rest/app.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const request = require('supertest');
 const app = require('./app')
 
-describe('GET all tasks', function() {
+describe('GET: all tasks', function() {
     test('It should response 200.', (done) => {
         request(app).get('/api/v1.0/tasks').then(function(response) {
 	    expect(response.statusCode).toBe(200);
@@ -12,7 +12,7 @@ describe('GET all tasks', function() {
 });
 
 
-describe('GET single task', function() {
+describe('GET: single task', function() {
     test('It should return response 200.', (done) => {
         request(app).get('/api/v1.0/tasks/1').then(function(response) {
 	    expect(response.statusCode).toBe(200);
@@ -22,7 +22,7 @@ describe('GET single task', function() {
 });
 
 
-describe('POST/Create new task', function() {
+describe('POST: create new task', function() {
     test('It should return response 200.', (done) => {
         request(app).post('/api/v1.0/tasks').send({
 	    title: "Code Refactor",
@@ -34,7 +34,7 @@ describe('POST/Create new task', function() {
 });
 
 
-describe('GET created task', function() {
+describe('GET: created task', function() {
     test('It respond with json', function() {
 	return request(app)
 	    .get('/api/v1.0/tasks/5')
